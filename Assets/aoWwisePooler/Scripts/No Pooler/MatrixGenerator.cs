246441c3e038c65f99a4b143e475f010689d8492
aoleynikov678 <a.o.oleynikov@gmail.com>
2020-05-31 01:29:06 +0300

    simple boxes generator

---
+++ Assets/aoWwisePooler/Scripts/No Pooler/MatrixGenerator.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace ao.wwisepooler
+{
+    public class MatrixGenerator : MonoBehaviour
+    {
+        [SerializeField] private int count;
+        [SerializeField] private Transform pivot;
+        [SerializeField] private float radius;
+        [SerializeField] private float height;
+        
+        [SerializeField] private GameObject cellPrefab;
+
+        private void Awake()
+        {
+            var angleStepY = 180f / (count - 1);
+            var curAngleY = -90f;
+
+            for (var i = 0; i < count; i++)
+            {
+                GenerateCell(pivot.position, curAngleY, i);
+                curAngleY += angleStepY;
+            }
+        }
+
+        private void GenerateCell(Vector3 center, float angle, int counter)
+        {
+            var rad = Mathf.PI * angle / 180.0f;
+            var x = center.x + radius * Mathf.Sin(rad);
+            var z = center.z + radius * Mathf.Cos(rad);
+            var cell = Instantiate(cellPrefab, transform, true);
+            cell.transform.position = new Vector3(x, height, z);
+
+            var input = cell.GetComponent<IHaveInput>();
+            if (input != null && counter < 9)
+            {
+                input.SetInputKey(KeyCode.Alpha1 + counter);
+            }
+        }
+    }
+}
--- Assets/aoWwisePooler/Scripts/No Pooler/SoundBox.cs
+++ Assets/aoWwisePooler/Scripts/No Pooler/SoundBox.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 namespace ao.wwisepooler
 {
-    public class SoundBox : MonoBehaviour
+    public class SoundBox : MonoBehaviour, IHaveInput
     {
         [SerializeField] private AK.Wwise.Event audioEvent;
+        [SerializeField] private KeyCode inputKey;
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(inputKey))
             {
                 audioEvent.Post(gameObject);
             }
         }
+
+        public void SetInputKey(KeyCode keyCode)
+        {
+            inputKey = keyCode;
+        }
+    }
+
+    public interface IHaveInput
+    {
+        void SetInputKey(KeyCode keyCode);
     }
 }
